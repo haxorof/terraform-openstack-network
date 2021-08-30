5549720f8295dbba874619f701d2f7f6750bdbe6
Bjorn Oscarsson <haxorof@users.noreply.github.com>
2021-08-30 09:56:36 +0200

    Added terraform required providers

--- main.tf
+++ main.tf
@@ -1,5 +1,9 @@
 terraform {
-  required_version = ">= 0.12"
+  required_providers {
+    openstack = {
+      source = "terraform-provider-openstack/openstack"
+    }
+  }
 }
 
 resource "random_id" "this" {
